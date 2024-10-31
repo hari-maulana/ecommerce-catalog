d2e920065210943d224df06b97f246e101e5b83e
maulanahari <harimaulana.xyz@gmail.com>
2024-10-31 15:12:49 +0700

    Add swagger in routes

--- server/src/routes/cartRoutes.ts
+++ server/src/routes/cartRoutes.ts
@@ -4,7 +4,86 @@ import { CartController } from "../comtrollers/cartController";
 const router = Router();
 const cartController = new CartController();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     CartItem:
+ *       type: object
+ *       required:
+ *         - productId
+ *         - quantity
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: Auto-generated cart item ID
+ *         productId:
+ *           type: integer
+ *           description: Product ID
+ *         quantity:
+ *           type: integer
+ *           description: Quantity of product
+ */
+
+/**
+ * @swagger
+ * /cart:
+ *   get:
+ *     summary: Get all items in cart
+ *     responses:
+ *       200:
+ *         description: List of cart items
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   name:
+ *                     type: string
+ *                   image:
+ *                     type: string
+ *                   description:
+ *                     type: string
+ *                   price:
+ *                     type: number
+ *                   quantity:
+ *                     type: integer
+ *                   total:
+ *                     type: number
+ */
 router.get("/", cartController.getCart);
+
+/**
+ * @swagger
+ * /cart:
+ *   post:
+ *     summary: Add item to cart
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - productId
+ *               - quantity
+ *             properties:
+ *               productId:
+ *                 type: integer
+ *               quantity:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Added cart item
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CartItem'
+ */
 router.post("/", cartController.addToCart);
 
 export default router;
--- server/src/routes/productRoutes.ts
+++ server/src/routes/productRoutes.ts
@@ -4,8 +4,103 @@ import { ProductController } from "../comtrollers/productController";
 const router = Router();
 const productController = new ProductController();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Product:
+ *       type: object
+ *       required:
+ *         - name
+ *         - price
+ *         - category
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: Auto-generated product ID
+ *         name:
+ *           type: string
+ *           description: Product name
+ *         image:
+ *           type: string
+ *           description: Product image URL
+ *         price:
+ *           type: number
+ *           description: Product price
+ *         description:
+ *           type: string
+ *           description: Product description
+ *         category:
+ *           type: string
+ *           description: Product category
+ */
+
+/**
+ * @swagger
+ * /products:
+ *   get:
+ *     summary: Get all products
+ *     parameters:
+ *       - in: query
+ *         name: category
+ *         schema:
+ *           type: string
+ *         description: Filter products by category
+ *     responses:
+ *       200:
+ *         description: List of products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ */
 router.get("/", productController.getAllProducts);
+
+/**
+ * @swagger
+ * /products/{id}:
+ *   get:
+ *     summary: Get a product by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Product ID
+ *     responses:
+ *       200:
+ *         description: Product details
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       404:
+ *         description: Product not found
+ */
 router.get("/:id", productController.getProductById);
+
+/**
+ * @swagger
+ * /products:
+ *   post:
+ *     summary: Create a new product
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Product'
+ *     responses:
+ *       200:
+ *         description: Created product
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ */
 router.post("/", productController.createProduct);
 
 export default router;
